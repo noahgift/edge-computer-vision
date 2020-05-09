5af4d78e475aace6e240d5a8bbdd8d324a34885d
Noah Gift <noah.gift@gmail.com>
2020-05-09 02:17:29 +0000

    adding files

--- detect.py
+++ detect.py
@@ -1,14 +1,31 @@
+#!/usr/bin/env python
+import click
 import boto3
 
-#resource = boto3.resource("s3")
-rekognition = boto3.client("rekognition")
-response = rekognition.detect_labels(
-		Image={
-			"S3Object": {
-				"Bucket": "dec27computervision",
-				"Name": "fig_7_titan_small.jpg",
-			}
-		},
-	)
+@click.command()
+@click.option('--bucket', prompt='S3 Bucket',
+              help='This is the S3 Bucket')
+@click.option('--name', prompt='this is the name of the image',
+              help="Pass in the name:  i.e. husky.png")
+def labels(bucket, name):
+	"""This takes an S3 bucket and a image name"""
+	
+	print(f"This is the bucketname {bucket} !")
+	print(f"This is the imagename {name} !")
+	rekognition = boto3.client("rekognition")
+	response = rekognition.detect_labels(
+			Image={
+				"S3Object": {
+					"Bucket": bucket,
+					"Name": name,
+				}
+			},
+		)
+	labels = response['Labels']
+	click.echo(click.style('Found Labels:', fg='red'))
+	for label in labels:
+		click.echo(click.style(f'{label}', bg='blue', fg='white'))
 
-print(response)
+if __name__ == '__main__':
+    #pylint: disable=no-value-for-parameter
+    labels()
